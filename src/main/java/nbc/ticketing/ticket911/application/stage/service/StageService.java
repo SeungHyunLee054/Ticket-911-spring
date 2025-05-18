4904194eea6bad178e42895f4b36e3e49f7515f1
kibeo <kibeom0806@gmail.com>
2025-05-19 01:21:58 +0900

    Refactor: 팩토리메서드 수정

--- src/main/java/nbc/ticketing/ticket911/application/stage/service/StageService.java
+++ src/main/java/nbc/ticketing/ticket911/application/stage/service/StageService.java
@@ -1,9 +1,5 @@
 package nbc.ticketing.ticket911.application.stage.service;
 
-import nbc.ticketing.ticket911.domain.stage.dto.request.UpdateStageRequestDto;
-import nbc.ticketing.ticket911.domain.stage.dto.response.StageResponseDto;
-import nbc.ticketing.ticket911.domain.stage.exception.StageException;
-import nbc.ticketing.ticket911.domain.stage.exception.code.StageExceptionCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,7 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import nbc.ticketing.ticket911.domain.stage.dto.request.CreateStageRequestDto;
+import nbc.ticketing.ticket911.domain.stage.dto.request.UpdateStageRequestDto;
+import nbc.ticketing.ticket911.domain.stage.dto.response.StageResponseDto;
 import nbc.ticketing.ticket911.domain.stage.entity.Stage;
+import nbc.ticketing.ticket911.domain.stage.exception.StageException;
+import nbc.ticketing.ticket911.domain.stage.exception.code.StageExceptionCode;
 import nbc.ticketing.ticket911.domain.stage.repository.StageRepository;
 import nbc.ticketing.ticket911.domain.stage.status.Status;
 
@@ -32,51 +32,35 @@ public class StageService {
 
 		Stage savedStage = stageRepository.save(stage);
 
-		return StageResponseDto.builder()
-			.stageName(savedStage.getStageName())
-			.totalSeats(savedStage.getTotalSeat())
-			.status(savedStage.getStatus())
-			.build();
+		return StageResponseDto.from(savedStage);
 	}
 
 	public Page<StageResponseDto> getStages(String keyword, Pageable pageable) {
 		Page<Stage> stagePage = stageRepository.findByStageNameContaining(keyword, pageable);
-		return stagePage.map(stage -> StageResponseDto.builder()
-			.stageName(stage.getStageName())
-			.totalSeats(stage.getTotalSeat())
-			.status(stage.getStatus())
-			.build());
+		return stagePage.map(StageResponseDto::from);
 	}
 
 	public StageResponseDto getStage(Long stageId) {
 		Stage stage = verifyStage(stageId);
-		return StageResponseDto.builder()
-				.stageName(stage.getStageName())
-				.totalSeats(stage.getTotalSeat())
-				.status(stage.getStatus())
-				.build();
-	}
-
-	private Stage verifyStage(Long stageId){
-		return stageRepository.findById(stageId)
-				.orElseThrow(() -> new StageException(StageExceptionCode.STAGE_NOT_FOUND));
+		return StageResponseDto.from(stage);
 	}
 
 	@Transactional
 	public StageResponseDto updateService(Long stageId, UpdateStageRequestDto updateStageRequestDto) {
 		Stage stage = verifyStage(stageId);
 
-		if(updateStageRequestDto.getStageName() != null){
+		if (updateStageRequestDto.getStageName() != null) {
 			stage.updateStageName(updateStageRequestDto.getStageName());
 		}
-		if(updateStageRequestDto.getStatus() != null){
+		if (updateStageRequestDto.getStatus() != null) {
 			stage.updateStatus(updateStageRequestDto.getStatus());
 		}
 
-		return StageResponseDto.builder()
-				.stageName(stage.getStageName())
-				.totalSeats(stage.getTotalSeat())
-				.status(stage.getStatus())
-				.build();
+		return StageResponseDto.from(stage);
+	}
+
+	private Stage verifyStage(Long stageId) {
+		return stageRepository.findById(stageId)
+			.orElseThrow(() -> new StageException(StageExceptionCode.STAGE_NOT_FOUND));
 	}
 }
--- src/main/java/nbc/ticketing/ticket911/domain/stage/dto/response/StageResponseDto.java
+++ src/main/java/nbc/ticketing/ticket911/domain/stage/dto/response/StageResponseDto.java
@@ -2,12 +2,21 @@ package nbc.ticketing.ticket911.domain.stage.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import nbc.ticketing.ticket911.domain.stage.entity.Stage;
 import nbc.ticketing.ticket911.domain.stage.status.Status;
 
 @Getter
 @Builder
 public class StageResponseDto {
-    private String stageName;
-    private Long totalSeats;
-    private Status status;
+	private String stageName;
+	private Long totalSeats;
+	private Status status;
+
+	public static StageResponseDto from(Stage stage) {
+		return StageResponseDto.builder()
+			.stageName(stage.getStageName())
+			.totalSeats(stage.getTotalSeat())
+			.status(stage.getStatus())
+			.build();
+	}
 }
