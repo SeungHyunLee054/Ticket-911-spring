bd8a67b147df3dd4c8a68e6c5378e6b313e86654
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-19 18:35:17 +0900

    Fix: whitelist 추가
    swagger에서 계속 인증 오류를 발생해서 url 추가

--- src/main/java/nbc/ticketing/ticket911/infrastructure/security/jwt/constant/JwtConstants.java
+++ src/main/java/nbc/ticketing/ticket911/infrastructure/security/jwt/constant/JwtConstants.java
@@ -10,6 +10,6 @@ public class JwtConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String KEY_ROLES = "roles";
 	public static final List<String> PUBLIC_URLS = List.of(
-		"/auth/signin", "/users/signup", "/swagger-ui", "/swagger-ui.html", "/v3/api-docs"
+		"/auth/signin", "/users/signup", "/swagger-ui", "/swagger-ui.html", "/v3/api-docs", "/swagger-resources", "/webjars"
 	);
 }
