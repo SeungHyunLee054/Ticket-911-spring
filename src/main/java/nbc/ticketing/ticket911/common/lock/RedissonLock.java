e17fb35401933c174fe0db6e361b7519c4248cad
SeungHyunLee054 <yellow13lsh@naver.com>
2025-05-20 20:19:07 +0900

    Feat: redisson 분산락 어노테이션
    redisson으로 분산락을 적용할 때 AOP로 기능할 수 있도록 어노테이션 생성

---
+++ src/main/java/nbc/ticketing/ticket911/common/lock/RedissonLock.java
@@ -0,0 +1,20 @@
+package nbc.ticketing.ticket911.common.lock;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RedissonLock {
+	String key();
+
+	String group() default "";
+
+	long waitTime() default 3L;
+
+	long leaseTime() default 2L;
+}
