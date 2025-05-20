4ef5a01604bbf0d4e6b6df32de35added3bca0fd
thezz9 <thezz9@naver.com>
2025-05-20 11:27:09 +0900

    feat: Booking 관련 도메인 메서드 추가

--- src/main/java/nbc/ticketing/ticket911/domain/booking/service/BookingDomainService.java
+++ src/main/java/nbc/ticketing/ticket911/domain/booking/service/BookingDomainService.java
@@ -1,4 +1,28 @@
 package nbc.ticketing.ticket911.domain.booking.service;
 
+import org.springframework.stereotype.Component;
+
+import nbc.ticketing.ticket911.domain.booking.entity.Booking;
+import nbc.ticketing.ticket911.domain.booking.exception.BookingException;
+import nbc.ticketing.ticket911.domain.booking.exception.code.BookingExceptionCode;
+import nbc.ticketing.ticket911.domain.user.entity.User;
+
+@Component
 public class BookingDomainService {
+
+	public void pay(User user, Booking booking) {
+		int totalPrice = booking.getTotalPrice();
+
+		if (user.getPoint() < totalPrice) {
+			throw new BookingException(BookingExceptionCode.PAYMENT_FAILED);
+		}
+
+		user.usePoint(totalPrice);
+	}
+
+	public void cancelBooking(User user, Booking booking) {
+		int refundPoint = booking.getTotalPrice();
+		user.refundPoint(refundPoint);
+		booking.canceledBooking();
+	}
 }
--- src/main/java/nbc/ticketing/ticket911/domain/concertseat/service/ConcertSeatDomainService.java
+++ src/main/java/nbc/ticketing/ticket911/domain/concertseat/service/ConcertSeatDomainService.java
@@ -1,14 +1,17 @@
 package nbc.ticketing.ticket911.domain.concertseat.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import nbc.ticketing.ticket911.domain.booking.exception.BookingException;
+import nbc.ticketing.ticket911.domain.booking.exception.code.BookingExceptionCode;
+import nbc.ticketing.ticket911.domain.concert.entity.Concert;
 import nbc.ticketing.ticket911.domain.concert.exception.ConcertException;
 import nbc.ticketing.ticket911.domain.concert.exception.code.ConcertExceptionCode;
 import nbc.ticketing.ticket911.domain.concert.repository.ConcertRepository;
 import nbc.ticketing.ticket911.domain.concertseat.entity.ConcertSeat;
-import nbc.ticketing.ticket911.domain.concertseat.exception.ConcertSeatException;
-import nbc.ticketing.ticket911.domain.concertseat.exception.code.ConcertSeatExceptionCode;
 
 @Component
 @RequiredArgsConstructor
@@ -22,15 +25,29 @@ public class ConcertSeatDomainService {
 		}
 	}
 
-	public void reserve(ConcertSeat seat) {
-		if (seat.isReserved()) {
-			throw new ConcertSeatException(ConcertSeatExceptionCode.ALREADY_BOOKED);
+	public void validateAllSameConcert(Concert concert, List<ConcertSeat> concertSeats) {
+
+		boolean allSameConcert = concertSeats.stream()
+			.allMatch(seat -> seat.getConcertId().equals(concert.getId()));
+
+		if (!allSameConcert) {
+			throw new BookingException(BookingExceptionCode.DIFFERENT_CONCERTS_NOT_ALLOWED);
 		}
-		seat.markReserved(); // 엔티티는 단순 상태 전환 메서드만 보유
 	}
 
-	public void cancel(ConcertSeat seat) {
-		seat.markUnreserved();
+	public void validateNotReserved(List<ConcertSeat> concertSeats) {
+		concertSeats.forEach(seat -> {
+			if (seat.isReserved()) {
+				throw new BookingException(BookingExceptionCode.SEAT_ALREADY_RESERVED);
+			}
+		});
 	}
 
+	public void reserveAll(List<ConcertSeat> concertSeats) {
+		concertSeats.forEach(ConcertSeat::reserve);
+	}
+
+	public void cancelAll(List<ConcertSeat> concertSeats) {
+		concertSeats.forEach(ConcertSeat::cancelReservation);
+	}
 }
