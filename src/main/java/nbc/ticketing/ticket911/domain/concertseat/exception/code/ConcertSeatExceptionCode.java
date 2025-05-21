352a5a25e160ef3f63a91a19579d71f90faa0f8d
thezz9 <thezz9@naver.com>
2025-05-21 18:33:15 +0900

    refactor: 예외코드 분리

--- src/main/java/nbc/ticketing/ticket911/domain/concertseat/exception/code/ConcertSeatExceptionCode.java
+++ src/main/java/nbc/ticketing/ticket911/domain/concertseat/exception/code/ConcertSeatExceptionCode.java
@@ -8,8 +8,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ConcertSeatExceptionCode {
-	ALREADY_BOOKED(false, HttpStatus.BAD_REQUEST, "이미 예약된 좌석입니다."),
-	CONCERT_SEAT_NOT_FOUND(false, HttpStatus.CONFLICT, "공연 좌석 없음");
+
+	INVALID_SEAT_SELECTION(false, HttpStatus.NOT_FOUND, "선택된 좌석이 존재하지 않습니다. 좌석 정보를 확인해주세요."),
+	DIFFERENT_CONCERTS_NOT_ALLOWED(false, HttpStatus.BAD_REQUEST, "서로 다른 공연의 좌석은 동시에 예매할 수 없습니다."),
+	SEAT_ALREADY_RESERVED(false, HttpStatus.CONFLICT, "이미 예약된 좌석입니다.");
+
 	private final boolean success;
 	private final HttpStatus status;
 	private final String message;
