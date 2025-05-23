54f0a00ff43b60d466f99033bfab86838a20dfff
thezz9 <thezz9@naver.com>
2025-05-23 11:38:15 +0900

    refactor: Redisson 리팩토링, 함수형 인터페이스 추가

--- src/main/java/nbc/ticketing/ticket911/domain/lock/LockRedisService.java
+++ src/main/java/nbc/ticketing/ticket911/domain/lock/LockRedisService.java
@@ -1,10 +1,11 @@
 package nbc.ticketing.ticket911.domain.lock;
 
 import java.util.List;
-import java.util.concurrent.Callable;
+
+import nbc.ticketing.ticket911.common.funtional.ThrowingSupplier;
 
 public interface LockRedisService {
-	<T> T executeWithLock(String key, long waitTime, long leaseTime, Callable<T> action);
+	<T> T executeWithLock(String key, long waitTime, long leaseTime, ThrowingSupplier<T> action);
 
-	<T> T executeWithMultiLock(List<String> key, long waitTime, long leaseTime, Callable<T> action);
+	<T> T executeWithMultiLock(List<String> key, long waitTime, long leaseTime, ThrowingSupplier<T> action);
 }
--- src/main/java/nbc/ticketing/ticket911/infrastructure/redisson/RedissonLockRedisService.java
+++ src/main/java/nbc/ticketing/ticket911/infrastructure/redisson/RedissonLockRedisService.java
@@ -2,11 +2,11 @@ package nbc.ticketing.ticket911.infrastructure.redisson;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import nbc.ticketing.ticket911.common.funtional.ThrowingSupplier;
 import nbc.ticketing.ticket911.domain.lock.LockRedisRepository;
 import nbc.ticketing.ticket911.domain.lock.LockRedisService;
 import nbc.ticketing.ticket911.infrastructure.redisson.exception.LockRedisException;
@@ -19,16 +19,22 @@ public class RedissonLockRedisService implements LockRedisService {
 	private final LockRedisRepository lockRedisRepository;
 
 	@Override
-	public <T> T executeWithLock(String key, long waitTime, long leaseTime, Callable<T> action) {
+	public <T> T executeWithLock(String key, long waitTime, long leaseTime, ThrowingSupplier<T> action) {
 		boolean locked = lockRedisRepository.lock(key, waitTime, leaseTime);
 		if (!locked) {
 			throw new LockRedisException(LockRedisExceptionCode.LOCK_TIMEOUT);
 		}
 		try {
-			return action.call();
-		} catch (RuntimeException re) {
-			throw re;
-		} catch (Exception e) {
+			return action.get();
+		} catch (LockRedisException lre) {
+			throw lre;
+		} catch (Throwable t) {
+			if (t instanceof RuntimeException re) {
+				throw re;
+			}
+			if (t instanceof Error err) {
+				throw err;
+			}
 			throw new LockRedisException(LockRedisExceptionCode.LOCK_PROCEED_FAIL);
 		} finally {
 			lockRedisRepository.unlock(key);
@@ -36,7 +42,8 @@ public class RedissonLockRedisService implements LockRedisService {
 	}
 
 	@Override
-	public <T> T executeWithMultiLock(List<String> keys, long waitTime, long leaseTime, Callable<T> action) {
+	public <T> T executeWithMultiLock(List<String> keys, long waitTime, long leaseTime, ThrowingSupplier<T> action)
+		throws LockRedisException {
 		List<String> lockedKeys = new ArrayList<>();
 
 		try {
@@ -47,10 +54,16 @@ public class RedissonLockRedisService implements LockRedisService {
 				}
 				lockedKeys.add(key);
 			}
-			return action.call();
-		} catch (RuntimeException re) {
-			throw re;
-		} catch (Exception e) {
+			return action.get();
+		} catch (LockRedisException lre) {
+			throw lre;
+		} catch (Throwable t) {
+			if (t instanceof RuntimeException re) {
+				throw re;
+			}
+			if (t instanceof Error err) {
+				throw err;
+			}
 			throw new LockRedisException(LockRedisExceptionCode.LOCK_PROCEED_FAIL);
 		} finally {
 			for (String key : lockedKeys) {
